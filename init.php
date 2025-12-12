f71ca2b7001c5dc6b5d194b6855d4c3c47f822f4
supahgreg <supahgreg@users.noreply.github.com>
2025-12-12 04:58:28 +0000

    Fix the value of 'X-Accel-Redirect'.
    Prior to this the plugin wasn't working due to an absolute path, which would never start with 'cache/', getting passed in.
    
    See https://github.com/tt-rss/tt-rss/issues/205

--- init.php
+++ init.php
@@ -2,37 +2,45 @@
 class Nginx_Xaccel extends Plugin {
 
 	function about() {
-		return array(1.0,
-			"Sends static files via nginx X-Accel-Redirect header",
-			"fox",
+		return [
+			1.1,
+			'Sends static files via nginx X-Accel-Redirect header',
+			'fox',
 			true,
-			"https://dev.tt-rss.org/fox/ttrss-nginx-xaccel");
+			'https://github.com/tt-rss/tt-rss-plugin-nginx-xaccel',
+		];
 	}
 
 	function init($host) {
-		Config::add("NGINX_XACCEL_PREFIX", "/tt-rss");
+		// In the official Docker setup this corresponds to the web-nginx container's APP_BASE.
+		Config::add('NGINX_XACCEL_PREFIX', '/tt-rss');
 
 		$host->add_hook($host::HOOK_SEND_LOCAL_FILE, $this);
 	}
 
+	/**
+	 * @param string $filename The full path of the file to send.
+	 * @return bool true if the plugin handled the request, false otherwise
+	 */
 	function hook_send_local_file($filename) {
+		// Strip the base/self directory from the full path to get something like '/cache/x/y'.
+		// For the sake of simplicity we'll assume the default of 'cache' is used for CACHE_DIR.
+		$cache_path = mb_substr($filename, mb_strlen(Config::get_self_dir()));
 
-		if (Config::get("NGINX_XACCEL_PREFIX") && mb_strpos($filename, "cache/") === 0) {
-
+		if (Config::get('NGINX_XACCEL_PREFIX') && str_starts_with($cache_path, '/cache/')) {
 			$mimetype = mime_content_type($filename);
 
 			// this is hardly ideal but 1) only media is cached in images/ and 2) seemingly only mp4
 			// video files are detected as octet-stream by mime_content_type()
-
-			if ($mimetype == "application/octet-stream")
-				$mimetype = "video/mp4";
+			if ($mimetype == 'application/octet-stream')
+				$mimetype = 'video/mp4';
 
 			header("Content-type: $mimetype");
 
-			$stamp = gmdate("D, d M Y H:i:s", filemtime($filename)) . " GMT";
+			$stamp = gmdate('D, d M Y H:i:s', filemtime($filename)) . ' GMT';
 			header("Last-Modified: $stamp", true);
 
-			header("X-Accel-Redirect: " . Config::get("NGINX_XACCEL_PREFIX") . "/" . $filename);
+			header('X-Accel-Redirect: ' . Config::get('NGINX_XACCEL_PREFIX') . $cache_path);
 
 			return true;
 		}
@@ -43,6 +51,4 @@ class Nginx_Xaccel extends Plugin {
 	function api_version() {
 		return 2;
 	}
-
 }
-?>
